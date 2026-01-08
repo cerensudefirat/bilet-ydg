c602d76a2f798a90e374a8d7b7bc6cd11ee6ed42
Ceren Sude Fırat <cerensd586@gmail.com>
2026-01-08 15:34:31 +0300

    test

--- src/test/java/com/ydg/bilet/e2e/Senaryo7StokIT.java
+++ src/test/java/com/ydg/bilet/e2e/Senaryo7StokIT.java
@@ -39,10 +39,6 @@ public class Senaryo7StokIT {
         }
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
-
-
-
-
     @AfterEach
     void tearDown() { if (driver != null) driver.quit(); }
 
